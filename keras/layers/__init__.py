0a1ab845ae9e0a1884c5a5300124eecc4ddc36b8
beareme <bremeseiro@udc.es>
2018-03-02 18:32:48 +0100

    Added MultiTaskUncertainty layer

--- keras/layers/__init__.py
+++ keras/layers/__init__.py
@@ -10,6 +10,7 @@ from .core import *
 from .convolutional import *
 from .pooling import *
 from .local import *
+from .loss_layers import *
 from .recurrent import *
 from .cudnn_recurrent import *
 from .normalization import *
