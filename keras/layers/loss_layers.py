ecf432bd77b4af037f031f06df9b80b9af066f76
beareme <bremeseiro@udc.es>
2018-03-05 16:27:18 +0100

    Modified Uncertainty Multitask loss layer

--- keras/layers/loss_layers.py
+++ keras/layers/loss_layers.py
@@ -5,28 +5,20 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-import warnings
 
 from .. import backend as K
-from .. import activations
-from .. import initializers
-from .. import regularizers
-from .. import constraints
-from ..engine import Layer
-from ..engine import InputSpec
-from ..utils.generic_utils import has_arg
 
-# Legacy support.
-from ..legacy.layers import Recurrent
-from ..legacy import interfaces
+from ..engine import Layer
+
 
 
 # Custom loss layer
 class CustomMultiLossLayer(Layer):
-    def __init__(self, nb_outputs=2, **kwargs):
+    def __init__(self, nb_outputs=2, loss_functions=[], **kwargs):
         self.nb_outputs = nb_outputs
+        self.loss_functions = loss_functions
         self.is_placeholder = True
+        assert len(loss_functions) == nb_outputs
         super(CustomMultiLossLayer, self).__init__(**kwargs)
 
     def build(self, input_shape=None):
@@ -34,15 +26,15 @@ class CustomMultiLossLayer(Layer):
         self.log_vars = []
         for i in range(self.nb_outputs):
             self.log_vars += [self.add_weight(name='log_var' + str(i), shape=(1,),
-                                              initializer=Constant(0.), trainable=True)]
+                                              initializer='zeros', trainable=True)]
         super(CustomMultiLossLayer, self).build(input_shape)
 
     def multi_loss(self, ys_true, ys_pred):
         assert len(ys_true) == self.nb_outputs and len(ys_pred) == self.nb_outputs
         loss = 0
-        for y_true, y_pred, log_var in zip(ys_true, ys_pred, self.log_vars):
+        for y_true, y_pred, log_var, loss_func in zip(ys_true, ys_pred, self.log_vars, self.loss_functions):
             precision = K.exp(-log_var[0])
-            loss += K.sum(precision * (y_true - y_pred) ** 2. + log_var[0], -1)
+            loss += K.sum(precision * loss_func(y_true,y_pred) + log_var[0], -1)
         return K.mean(loss)
 
     def call(self, inputs):
@@ -52,3 +44,11 @@ class CustomMultiLossLayer(Layer):
         self.add_loss(loss, inputs=inputs)
         # We won't actually use the output.
         return K.concatenate(inputs, -1)
+
+    def get_config(self):
+        config = {
+            'nb_outputs': self.nb_outputs,
+            'loss_functions': self.loss_functions,
+        }
+        base_config = super(Layer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
