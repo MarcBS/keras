819d63f90a034e2b776afb0a7b597975cd9e125b
lvapeab <lvapeab@gmail.com>
2018-10-18 12:47:13 +0200

    AdamAccumulate optimizer

--- keras/optimizers.py
+++ keras/optimizers.py
@@ -660,6 +660,83 @@ class Adam(Optimizer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+class AdamAccumulate(Optimizer):
+    def __init__(self, lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.,
+                 amsgrad=False, accum_iters=20, **kwargs):
+        super(AdamAccumulate, self).__init__(**kwargs)
+        with K.name_scope(self.__class__.__name__):
+            self.iterations = K.variable(0, dtype='int64', name='iterations')
+            self.lr = K.variable(lr, name='lr')
+            self.beta_1 = K.variable(beta_1, name='beta_1')
+            self.beta_2 = K.variable(beta_2, name='beta_2')
+            self.decay = K.variable(decay, name='decay')
+        if epsilon is None:
+            epsilon = K.epsilon()
+        self.epsilon = epsilon
+        self.initial_decay = decay
+        self.amsgrad = amsgrad
+        self.accum_iters = K.variable(accum_iters, dtype='int64')
+
+    @interfaces.legacy_get_updates_support
+    def get_updates(self, loss, params, learning_rate_multipliers):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+        lr = self.lr
+        if self.initial_decay > 0:
+            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
+                                                      K.dtype(self.decay))))
+
+        t = K.cast(self.iterations, K.floatx()) + 1
+        lr_t = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) /
+                     (1. - K.pow(self.beta_1, t)))
+
+        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        gs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+
+        if self.amsgrad:
+            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        else:
+            vhats = [K.zeros(1) for _ in params]
+        self.weights = [self.iterations] + ms + vs + vhats
+
+        for p, g, m, v, vhat, gg in zip(params, grads, ms, vs, vhats, gs):
+
+            update_gradients = K.cast(K.equal(self.iterations % self.accum_iters, 0), K.floatx())
+            gg_t = (1 - update_gradients) * (gg + g)
+            m_t = (self.beta_1 * m) + (1. - self.beta_1) * (gg + update_gradients * g) / K.cast(self.accum_iters, K.floatx())
+            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square((gg + update_gradients * g) / K.cast(self.accum_iters, K.floatx()))
+            if self.amsgrad:
+                vhat_t = K.maximum(vhat, v_t)
+                p_t = p - lr_t * m_t / (K.sqrt(vhat_t) + self.epsilon)
+                self.updates.append(K.update(vhat, vhat_t))
+            else:
+                p_t = p - lr_t * m_t / (K.sqrt(v_t) + self.epsilon)
+
+            self.updates.append((m, update_gradients * m_t + (1 - update_gradients) * m))
+            self.updates.append((v, update_gradients * v_t + (1 - update_gradients) * v))
+            self.updates.append((gg, gg_t))
+            new_p = p_t
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {'lr': float(K.get_value(self.lr)),
+                  'beta_1': float(K.get_value(self.beta_1)),
+                  'beta_2': float(K.get_value(self.beta_2)),
+                  'decay': float(K.get_value(self.decay)),
+                  'epsilon': self.epsilon,
+                  'amsgrad': self.amsgrad,
+                  'accum_iters': float(K.get_value(self.accum_iters))}
+        base_config = super(AdamAccumulate, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
 class Adamax(Optimizer):
     """Adamax optimizer from Adam paper's Section 7.
 
@@ -864,6 +941,7 @@ rmsprop = RMSprop
 adagrad = Adagrad
 adadelta = Adadelta
 adam = Adam
+adamaccumulate = AdamAccumulate
 adamax = Adamax
 nadam = Nadam
 
