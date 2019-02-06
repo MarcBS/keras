ae8734097aba51f4dfd4421c419e29254ec4d3e2
lvapeab <lvapeab@gmail.com>
2019-02-06 18:01:45 +0100

    Sync TensorBoardCallback with master

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -1041,7 +1041,6 @@ class TensorBoard(Callback):
         self.log_dir = log_dir
         self.histogram_freq = histogram_freq
         self.merged = None
-        self.saved = False
         self.write_graph = write_graph
         self.write_grads = write_grads
         self.write_images = write_images
@@ -1128,41 +1127,36 @@ class TensorBoard(Callback):
         else:
             self.writer = tf.summary.FileWriter(self.log_dir)
 
-        if self.embeddings_freq:
+        if self.embeddings_freq and self.embeddings_data is not None:
+            self.embeddings_data = standardize_input_data(self.embeddings_data,
+                                                          model.input_names)
+
             embeddings_layer_names = self.embeddings_layer_names
 
             if not embeddings_layer_names:
                 embeddings_layer_names = [layer.name for layer in self.model.layers
                                           if type(layer).__name__ == 'Embedding']
+            self.assign_embeddings = []
+            embeddings_vars = {}
 
-            if self.embeddings_data is not None:
-                self.embeddings_data = standardize_input_data(self.embeddings_data, model.input_names)
-                self.assign_embeddings = []
-                embeddings_vars = {}
+            self.batch_id = batch_id = tf.placeholder(tf.int32)
+            self.step = step = tf.placeholder(tf.int32)
 
-                self.batch_id = batch_id = tf.placeholder(tf.int32)
-                self.step = step = tf.placeholder(tf.int32)
+            for layer in self.model.layers:
+                if layer.name in embeddings_layer_names:
+                    embedding_input = self.model.get_layer(layer.name).output
+                    embedding_size = np.prod(embedding_input.shape[1:])
+                    embedding_input = tf.reshape(embedding_input,
+                                                 (step, int(embedding_size)))
+                    shape = (self.embeddings_data[0].shape[0], int(embedding_size))
+                    embedding = tf.Variable(tf.zeros(shape),
+                                            name=layer.name + '_embedding')
+                    embeddings_vars[layer.name] = embedding
+                    batch = tf.assign(embedding[batch_id:batch_id + step],
+                                      embedding_input)
+                    self.assign_embeddings.append(batch)
 
-                for layer in self.model.layers:
-                    if layer.name in embeddings_layer_names:
-                        embedding_input = self.model.get_layer(layer.name).output
-                        embedding_size = np.prod(embedding_input.shape[1:])
-                        embedding_input = tf.reshape(embedding_input,
-                                                     (step, int(embedding_size)))
-                        shape = (self.embeddings_data[0].shape[0], int(embedding_size))
-                        embedding = tf.Variable(tf.zeros(shape),
-                                                name=layer.name + '_embedding')
-                        embeddings_vars[layer.name] = embedding
-                        batch = tf.assign(embedding[batch_id:batch_id + step],
-                                          embedding_input)
-                        self.assign_embeddings.append(batch)
-            else:
-                if not self.saved:
-                    embeddings_vars = {layer.name: layer.weights[0]
-                                       for layer in self.model.layers
-                                       if layer.name in embeddings_layer_names}
-                    self.saver = tf.train.Saver(list(embeddings_vars.values()))
-                    self.saved = True
+            self.saver = tf.train.Saver(list(embeddings_vars.values()))
 
             if not isinstance(self.embeddings_metadata, str):
                 embeddings_metadata = self.embeddings_metadata
@@ -1172,12 +1166,13 @@ class TensorBoard(Callback):
 
             config = projector.ProjectorConfig()
 
-            if not self.saved:
-                for layer_name, tensor in embeddings_vars.items():
-                    embedding = config.embeddings.add()
-                    embedding.tensor_name = tensor.name
-                    if layer_name in embeddings_metadata:
-                        embedding.metadata_path = embeddings_metadata[layer_name]
+            for layer_name, tensor in embeddings_vars.items():
+                embedding = config.embeddings.add()
+                embedding.tensor_name = tensor.name
+
+                if layer_name in embeddings_metadata:
+                    embedding.metadata_path = embeddings_metadata[layer_name]
+
             projector.visualize_embeddings(self.writer, config)
 
     def on_epoch_end(self, epoch, logs=None):
@@ -1186,9 +1181,9 @@ class TensorBoard(Callback):
         if not self.validation_data and self.histogram_freq:
             raise ValueError("If printing histograms, validation_data must be "
                              "provided, and cannot be a generator.")
-        # if self.embeddings_data is None and self.embeddings_freq:
-        #     raise ValueError("To visualize embeddings, embeddings_data must "
-        #                      "be provided.")
+        if self.embeddings_data is None and self.embeddings_freq:
+            raise ValueError("To visualize embeddings, embeddings_data must "
+                             "be provided.")
         if self.validation_data and self.histogram_freq:
             if epoch % self.histogram_freq == 0:
 
@@ -1256,11 +1251,6 @@ class TensorBoard(Callback):
                                     epoch)
 
                     i += self.batch_size
-        elif self.embeddings_data is None and self.embeddings_freq != 0:
-            if epoch % self.embeddings_freq == 0:
-                self.saver.save(self.sess,
-                                os.path.join(self.log_dir, 'keras_embedding.ckpt'),
-                                epoch)
 
         if self.update_freq == 'epoch':
             index = epoch
